1ed0d4347b2c764979bd12998a4b6ad260b4347b
Athar Munir <mamunir@gmail.com>
2020-08-21 22:04:32 -0600

    MyMethod

---
+++ gittest.cs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gittest.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
